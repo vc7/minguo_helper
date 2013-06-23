5dd3efc5dab93ac0d0093fcce3f142dd103206c2
Vincent <vince78718@gmail.com>
2013-06-23 22:50:35 +0800

    Language id's list of minguo_helper.

---
+++ language/english/minguo_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['year']			= '年';
+
+$lang['before_minguo'] 	= '民國前';
+$lang['after_minguo'] 	= '民國';
+
+$lang['before_christ'] 	= '西元前';
+$lang['anno_domini'] 	= '西元';
+
+/* End of file breadcrumb_lang.php */
+/* Location: ./application/language/english/breadcrumb_lang.php */
